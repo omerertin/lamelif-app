b1cb0560c0e3d3a60bc8d8cea2971a7a10559846
omerertin <ssscrazygamesss@gmail.com>
2026-03-27 21:05:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -312,4 +312,6 @@ def sw():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
